4d332b53cf2ac4520a60964797beaae1ca8f5cd9
Janakirami Reddi Koki <jkoki@onetrust.com>
2022-08-10 06:27:37 +0530

    first commit

---
+++ src/main/java/com/java/spring/SpringJpaPsqlApplication.java
@@ -0,0 +1,13 @@
+package com.java.spring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringJpaPsqlApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringJpaPsqlApplication.class, args);
+	}
+
+}
